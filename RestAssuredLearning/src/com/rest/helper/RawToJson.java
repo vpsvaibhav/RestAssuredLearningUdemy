efdb6bd1a1bd1cd0d238cb884b51961780f8fc11
in566vaison <in566vaison@CSPLAP0176.ext.crifnet.com>
2020-09-05 22:55:39 +0530

    New Classes added

---
+++ RestAssuredLearning/src/com/rest/helper/RawToJson.java
@@ -0,0 +1,12 @@
+
+package com.rest.helper;
+
+import io.restassured.path.json.JsonPath;
+
+public class RawToJson {
+	
+	public static JsonPath rawToJson (String rawResponse) {
+		JsonPath jsonPath = new JsonPath(rawResponse);
+		return jsonPath;
+	}
+}
